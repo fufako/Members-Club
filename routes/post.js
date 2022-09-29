8587f146a59754c67da60340009a9539508f13de
fufako <patryko.mikiciuk@gmail.com>
2022-09-29 17:35:49 +0200

    Format date

--- routes/post.js
+++ routes/post.js
@@ -1,15 +1,17 @@
 const express = require("express")
 const router = express.Router()
 const Post = require("../models/postModel")
+const { DateTime } = require("luxon")
 
 router.get("/", function (req, res, next) {
   res.render("create", { user: res.locals.currentUser })
 })
 router.post("/", (req, res) => {
+  let date = new Date()
   const post = new Post({
     content: req.body.content,
     author: res.locals.currentUser,
-    date: new Date(),
+    date: DateTime.fromJSDate(date).toFormat("yyyy-MM-dd, HH:mm"),
   }).save((err) => {
     if (err) {
       return next(err)
