7da4f8d124949626b6af0442cfa485fb8fa466a9
fufako <patryko.mikiciuk@gmail.com>
2022-09-30 11:06:38 +0200

    Add styling

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -15,10 +15,11 @@ exports.index = function (req, res, next) {
 }
 
 exports.member = function (req, res, next) {
-  res.render("member", { user: res.locals.currentUser })
+  res.render("member", { user: res.locals.currentUser, passGood: true })
 }
 exports.member_post = function (req, res, next) {
   if (req.body.password != process.env.MEMBER_PASS) {
+    res.render("member", { passGood: false, user: res.locals.currentUser })
   } else {
     User.findOne({ _id: res.locals.currentUser._id }, (err, user) => {
       user.status = "member"
